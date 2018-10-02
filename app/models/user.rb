c568aa235977f60445a3bed3b2bcb10f0cfefa03
Antoni Ruth <antoniruthschmuck@gmail.com>
2018-10-02 15:26:21 -0400

    Validadores iniciales para User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+
+  validates_presence_of :password_digest, :name, :lastName, :phone, :docId,
+                        :address, :admin
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should_not allow_value('myemail@email').for(:email) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:lastName) }
   it { should validate_presence_of(:docId) }
   it { should validate_presence_of(:phone) }
   it { should validate_presence_of(:address) }
